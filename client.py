7027279aeb39ff5c85f331d0cb535c46a0343dac
Christian Viazzo <via17001@byui.edu>
2022-03-05 17:58:47 -0700

    integrated spotify api calls with network message sending

--- client.py
+++ client.py
@@ -1,4 +1,7 @@
+from multiprocessing.sharedctypes import Value
 import socket
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 
 HEADER = 64
@@ -8,13 +11,94 @@ DISCONNECT_MESSAGE = "!DISCONNECT"
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
 
+def init_client_cred_manager():
+    with open('token-data_client.txt', 'r') as file:
+        cred_data = file.readlines()
 
-def main():
-    pass
+    scope = 'user-library-read user-read-playback-state user-modify-playback-state'
+
+    client_cred_manager = SpotifyClientCredentials(
+        client_id=cred_data[0].strip(), 
+        client_secret=cred_data[1].strip()
+    )
     
+    print(cred_data[0])
+
+    return client_cred_manager
+
+def main_display():
+    client_cred_manager = init_client_cred_manager()
+    sp = spotipy.Spotify(client_credentials_manager=client_cred_manager)
+    song_selection = dict()  
+
+    while True:
+        print()
+        print("==========================================")
+        print()
+        print("(1) Display currently playing song.")
+        print("(2) Search for a song.")
+        print("(3) Add song from last search to queue.")
+        print("(4) Quit and disconnect.")
+        print()
+        print("==========================================")
+        print("Please enter a selection:")
+        user_input = input("> ")
+
+        match user_input:
+            case "1":
+                print()
+                print("[CURRENTLY PLAYING]")
+                send("playing")
+            case "2":
+                song_selection = search_tracks(sp, song_selection)
+            case "3":
+                if len(song_selection) == 0:
+                    print("[Error] You must make a new search before you can select a song.")
+                    continue
+                track_id = select_track(song_selection)
+                print()
+                send(track_id)
+            case "4":
+                break    
+
+
+def select_track(song_selection):
+    for i in range(len(song_selection.keys())):
+        print(f"{i+1:3}", f"{song_selection[i+1][0]:60}", "|", f"{song_selection[i+1][1]:30}")
+
+    while True:
+        try:
+            user_select = int(input("Which song would you like you add to queue? \n> "))
+        except ValueError:
+            print("[ERROR] Please enter a valid number.")
+        else:
+            break
+
+    # need to do data validation on this later.
+    return song_selection[user_select][2]
+
+
+"""
+Calls track search endpoint and populates a dictionary with results.
+"""
+def search_tracks(sp, song_selection):
+    user_search = input("What is the name of the track you'd like to search for: \n> ")
+    results = sp.search(q='track:' + user_search, type='track', limit=20)
+    
+    # This is to make sure the search results refresh. If I implemented things differently then I probably wouldn't
+    # need to do something like this.
+    song_selection.clear()
+
+    print("  # | TRACK TITLE                                                  | ARTISTS")
+    print("----------------------------------------------------------------------------------------")
+
+    for idx, track in enumerate(results['tracks']['items']):
+        song_selection[idx+1] = [track['name'], track['album']['artists'][0]['name'], track['external_urls']['spotify']]
+        print(f"{idx+1:3}", "|", f"{track['name']:60}", "|", f"{track['album']['artists'][0]['name']:30}")
+
+    return song_selection
+
 
 def send(msg):
     message = msg.encode(FORMAT)
@@ -25,3 +109,12 @@ def send(msg):
     client.send(send_length)
     client.send(message)
     print(client.recv(2048).decode(FORMAT))
+
+
+try:
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(ADDR)
+
+    main_display()
+except ConnectionRefusedError:
+    print("[ERROR] Bad connection. You may have the wrong IP Address or the server may be down.")
--- server.py
+++ server.py
@@ -1,5 +1,8 @@
+from gettext import find
 import socket
 import threading
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 HEADER = 64
 PORT = 5050
@@ -11,10 +14,80 @@ DISCONNECT_MESSAGE = "!DISCONNECT"
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
+def init_auth_manager():
+    with open('token-data_server.txt', 'r') as file:
+        cred_data = file.readlines()
+
+    scope = 'user-library-read user-read-playback-state user-modify-playback-state user-read-currently-playing'
+
+    auth_manager = SpotifyOAuth(
+        client_id=cred_data[0].strip(), 
+        client_secret=cred_data[1].strip(), 
+        redirect_uri=cred_data[2].strip(), 
+        scope=scope)
+
+    return auth_manager
 
 
-def handle_client(conn, addr):
+"""
+Asks user for which playback device they'd like and returns the device id.
+"""
+def select_device(avail_devices):
+    device_names = [*avail_devices]
+    device_names = list(avail_devices.keys())
+
+    if len(device_names) == 0:
+        return
+
+    user_input = -1
+
+    while user_input-1 not in range(len(device_names)):
+        try:
+            print()
+            for i in range(len(device_names)):
+                print(f"({i+1}) {device_names[i]}")
+            print()
+
+            print("Enter the number that corresponds with your player.")
+            user_input = int(input("> "))
+        except ValueError:
+            print("[ERROR] Please enter a valid number.")
+
+
+    return avail_devices[device_names[user_input-1]]
+    
+
+"""
+Calls API to grab the available devices user can interact with.
+"""
+def get_avail_devices(sp):
+    avail_devices = dict()
+    results = sp.devices()
+    
+    # print(len(results['devices']))
+
+    if len(results['devices']) != 0:
+        for i in range(len(results['devices'])):
+            avail_devices[results['devices'][i]['name']] = results['devices'][i]['id']
+    else:
+        print("[ERROR] There are no available devices.")
+    
+    return avail_devices
+
+
+"""
+Plays a provided track on a provided device.
+"""    
+def play_track(sp, device_id, track_id):
+    uris_list = []
+    uris_list.append(track_id)
+
+    sp.start_playback(device_id=device_id, uris=uris_list)
+
+
+def handle_client(conn, addr, sp, device_id):
     print(f"[NEW CONNECTION] {addr} connected.")
+    # conn.send("[CONNECTED] You connected to the host".encode(FORMAT))
 
     connected = True
     while connected:
@@ -26,7 +99,24 @@ def handle_client(conn, addr):
                 connected = False
 
             print(f"[{addr}] {msg}")
-            conn.send("Msg received".encode(FORMAT))
+            match msg:
+                case "playing":
+                    track_info = sp.currently_playing()
+                    track_name = track_info['item']['name']
+                    track_artist = track_info['item']['album']['artists'][0]['name']
+                    track_album = track_info['item']['album']['name']
+
+                    conn.send(f"Name: {track_name} | Artist: {track_artist}  | Album: {track_album}".encode(FORMAT))
+                
+
+            if ("https://open.spotify.com/track/") in msg:
+                play_track(sp, device_id, msg)
+                track_info = sp.currently_playing()
+                track_name = track_info['item']['name']
+                track_artist = track_info['item']['album']['artists'][0]['name']
+                conn.send(f"[ADDED] ({track_name} by {track_artist}) added to queue.".encode(FORMAT))
+
+            
 
     conn.close()
 
@@ -34,11 +124,24 @@ def handle_client(conn, addr):
 def start():
     server.listen()
     print(f"[LISTENING] Server is listening on {SERVER}")
+
+    # Placed API build inside of start fuhnction for organization
+    auth_manager = init_auth_manager()
+    sp = spotipy.Spotify(auth_manager=auth_manager)
+
+    # Host must select device player when initializing server.
+    avail_devices = get_avail_devices(sp)
+    device_id = select_device(avail_devices)
+
     while True:
+        if device_id == None:
+            break
         conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread = threading.Thread(target=handle_client, args=(conn, addr, sp, device_id))
         thread.start()
         print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+    
+    print("[CLOSING] server is stopping...")
 
 
 print("[STARTING] server is starting...")
